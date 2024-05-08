9f4d1793c79c207a501ebea15ad16fed5120e5bb
tanutv123 <taitanvo2309@gmail.com>
2024-05-09 00:17:26 +0700

    Add some landing page for front-end

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RecipeComponent} from "./components/recipe/recipe.component";
+import {MealPlanComponent} from "./components/meal-plan/meal-plan.component";
+import {MealPlanBoughtComponent} from "./components/meal-plan-bought/meal-plan-bought.component";
+import {RecipeDetailComponent} from "./components/recipe/recipe-detail/recipe-detail.component";
+import {RecipeStepListComponent} from "./components/recipe-step-list/recipe-step-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RecipeComponent},
+  {path:'',
+  children: [
+    {path: 'meal-plan', component: MealPlanComponent},
+    {path: 'meal-plan-bought', component: MealPlanBoughtComponent},
+    {path: 'recipe-detail', component: RecipeDetailComponent},
+    {path: 'recipe-step-list', component: RecipeStepListComponent},
+  ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
