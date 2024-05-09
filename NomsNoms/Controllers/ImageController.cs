b9cc021b917dc6027bb7400ff495885bdb68b592
vsnghi95 <vsnghi95@gmail.com>
2024-05-10 03:52:18 +0700

    Set up Cloudinary and ImageController

---
+++ NomsNoms/Controllers/ImageController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NomsNoms.Entities;
+using NomsNoms.Interfaces;
+using System.Net;
+
+namespace NomsNoms.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private readonly IImageService _imageService;
+
+        public ImageController(IImageService imageService)
+        {
+            _imageService = imageService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadAsync(IFormFile file)
+        {
+            var result = await _imageService.UploadAsync(file);
+            if (result == null)
+            {
+                ModelState.AddModelError("Upload image", " Something went wrong");
+                return Problem("Something went wrong", null, (int)HttpStatusCode.InternalServerError);
+            }
+            return Ok(new { link = result.SecureUri.AbsoluteUri, publicId = result.PublicId });
+
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync([FromBody] RecipeImage image)
+        {
+            var result = await _imageService.DeletePhotoAsync(image.PublicId);
+            if (result == null)
+            {
+                ModelState.AddModelError("Delete image", "Something went wrong during deleting the image");
+                return Problem("Something went wrong", null, (int)HttpStatusCode.InternalServerError);
+            }
+            return Ok(new { message = "Image successfully deleted.", deletedPublicId = image.PublicId });
+        }
+        [HttpDelete("user-image")]
+        public async Task<IActionResult> DeleteAsync([FromBody] UserPhoto image)
+        {
+            var result = await _imageService.DeletePhotoAsync(image.PublicId);
+            if (result == null)
+            {
+                ModelState.AddModelError("Delete image", "Something went wrong during deleting the image");
+                return Problem("Something went wrong", null, (int)HttpStatusCode.InternalServerError);
+            }
+            return Ok(new { message = "Image successfully deleted.", deletedPublicId = image.PublicId });
+        }
+    }
+}
---
+++ NomsNoms/Interfaces/IImageService.cs
@@ -0,0 +1,11 @@
+﻿using CloudinaryDotNet.Actions;
+
+
+namespace NomsNoms.Interfaces
+{
+    public interface IImageService
+    {
+        Task<ImageUploadResult> UploadAsync(IFormFile file);
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+}
