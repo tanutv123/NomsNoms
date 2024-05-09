8d7f01c9a2635efa0cc70d372d75a2335154c32b
tanutv123 <taitanvo2309@gmail.com>
2024-05-09 23:44:12 +0700

    Add taste profile

--- NomsNoms/Data/Migrations/20240509164226_TasteProfileAdded.Designer.cs
+++ NomsNoms/Data/Migrations/20240509164226_TasteProfileAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NomsNoms.Data;
 
@@ -11,9 +12,11 @@ using NomsNoms.Data;
 namespace NomsNoms.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240509164226_TasteProfileAdded")]
+    partial class TasteProfileAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -580,7 +583,7 @@ namespace NomsNoms.Data.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("TasteProfiles");
+                    b.ToTable("TasteProfile");
                 });
 
             modelBuilder.Entity("NomsNoms.Entities.UserCollection", b =>
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,13 +11,18 @@ import { FilterComponent } from './components/recipe/filter/filter.component';
 import { MealPlanComponent } from './components/meal-plan/meal-plan.component';
 import { MealPlanBoughtComponent } from './components/meal-plan-bought/meal-plan-bought.component';
 import { RecommendComponent } from './components/recipe/recommend/recommend.component';
-import {Home, LucideAngularModule, Menu, UserCheck, File, RefreshCcw, TrendingUp, Wheat, Heart, Eye, Star  } from "lucide-angular";
+import {Home, LucideAngularModule, Menu, UserCheck, File, RefreshCcw, TrendingUp, Clock,Wheat, Heart, Eye, Star, Zap  } from "lucide-angular";
 import { TrendingComponent } from './components/recipe/trending/trending.component';
 import { RecipeDetailComponent } from './components/recipe/recipe-detail/recipe-detail.component';
-import { RecipeCardComponent } from './components/recipe/recipe-card/recipe-card.component';
 import { RecipeStepListComponent } from './components/recipe-step-list/recipe-step-list.component';
 // import function to register Swiper custom elements
 import { register } from 'swiper/element/bundle';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { RecipeListComponent } from './components/user/recipe-list/recipe-list.component';
+import {RecipeCardComponent} from "./components/recipe/recipe-card/recipe-card.component";
+import { RecipeCardProfileComponent } from './components/user/recipe-list/recipe-card-profile/recipe-card-profile.component';
 // register Swiper custom elements
 register();
 @NgModule({
@@ -35,12 +40,17 @@ register();
     TrendingComponent,
     RecipeDetailComponent,
     RecipeCardComponent,
-    RecipeStepListComponent
+    RecipeStepListComponent,
+    ProfileComponent,
+    RecipeListComponent,
+    RecipeCardProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LucideAngularModule.pick({File, Home, Menu, UserCheck, RefreshCcw, TrendingUp, Wheat, Heart, Eye, Star    })
+    FontAwesomeModule,
+    LucideAngularModule.pick({File, Home, Menu, UserCheck, RefreshCcw, TrendingUp, Wheat, Heart, Eye, Star, Clock, Zap }),
+    BrowserAnimationsModule
   ],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
