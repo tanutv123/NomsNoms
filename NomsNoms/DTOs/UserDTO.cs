1a89994aba7b20abefa87fcd33b0aa3c8a2468aa
tanutv123 <taitanvo2309@gmail.com>
2024-05-10 16:23:45 +0700

    Implment login function for front-end

--- NomsNoms/DTOs/UserDTO.cs
+++ NomsNoms/DTOs/UserDTO.cs
@@ -3,6 +3,7 @@
     public class UserDTO
     {
         public string Name { get; set; }
+        public string KnownAs { get; set; }
         public string Token { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
--- NomsNoms/Program.cs
+++ NomsNoms/Program.cs
@@ -82,7 +82,7 @@ app.UseCors(builder => builder
 .AllowAnyHeader()
 .AllowAnyMethod()
 .AllowCredentials()
-.WithOrigins("https://localhost:4200"));
+.WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
